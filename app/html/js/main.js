40e28e1ffbbd6e472d6685acfe042354916377f4
Wojciech A. Jurczyk <wajurczyk@gmail.com>
2015-02-05 21:13:03 +0100

    added panels and some basic js

--- app/html/js/main.js
+++ app/html/js/main.js
@@ -1,6 +1,22 @@
 $(function() {
-	$("#dialDuration").knob();
-	$("#dialMotorLeft").knob();
-	$("#dialMotorRight").knob();
-	$("#dialHeadPosition").knob();
+	var dialDuration = $("#dialDuration");
+	var dialLeft = $("#dialMotorLeft");
+	var dialRight = $("#dialMotorRight");
+	var dialHead = $("#dialHeadPosition");
+
+	dialDuration.knob();
+	dialLeft.knob();
+	dialRight.knob();
+	dialHead.knob();
+
+	$("#btnSubmit").click(function() {
+		onSubmitClicked();
+	});
+
+	var onSubmitClicked = function() {
+		var duration = parseFloat( dialDuration.val() );
+		var left = parseInt( dialLeft.val() );
+		var right parseInt( dialRight.val() );
+		var head = parseInt( dialHead.val() );
+	};
 });
