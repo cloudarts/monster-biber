ee0af18124bf787ed6461be55d8f35d4c3c2a79e
Wojciech A. Jurczyk <wajurczyk@gmail.com>
2015-02-05 22:07:56 +0100

    added node-serialport basics. needs to be tested with biber

--- app/app.js
+++ app/app.js
@@ -2,11 +2,28 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var argv = require('minimist')(process.argv.slice(2));
+
+if( !argv.s || !argv.p ) {
+	console.log("\n\nusage: node app -p [PORT] -s [SERIAL DEVICE]\n\n");
+	process.exit(1);
+}
+
+var SerialPort = require('serialport').SerialPort;
+var serialPort = new SerialPort(argv.s, { baudrate:115200 });
+
+serialPort.on("open", function() {
+	console.log("\n\nserial port open...");
+
+	serialPort.on('data', function(data) {
+		console.log("received data from serial: " + data);
+	});
+});
 
 app.use(express.static(__dirname + '/html'));
 
-http.listen(3000, function() {
-	console.log('http server listening on *.3000');
+http.listen(argv.p, function() {
+	console.log('http server listening on *.' + argv.p);
 });
 
 io.on('connection', function(socket) {
